eb1807eaf6f48d0cbd0af968b0e491f15dd6bc3e
PhNam24 <98546157+PhNam24@users.noreply.github.com>
2022-08-14 21:35:17 +0700

    first commit

---
+++ Hello_World.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define test int t; cin >> t; while (t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello PTIT.";
+    return 0;
+}
